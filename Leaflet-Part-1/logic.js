b7d785f6339ae82be341e57208cf78b4e11df151
NaderMoa <nadermoazezi@gmail.com>
2024-02-22 18:18:58 -0500

    fixed the colors

--- Leaflet-Part-1/logic.js
+++ Leaflet-Part-1/logic.js
@@ -1,7 +1,8 @@
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 d3.json(url).then(function (data) {
 createMymap(data)
-})
+
+console.log(data)
 function createMymap(earthquake){
 //base map
 let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -10,22 +11,38 @@ let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   
   let myMap = L.map("map",{
     center: [37.7749,-122.4194],
-    zoom: 5,
+    zoom: 5.5,
     layers: [street]
  })
+
+//  let scale = d3.scaleLinear().domain([0,700]).range(["green","orange"])
   L.geoJSON(earthquake, {pointToLayer: function(feature, coords){
     let mag = feature.properties.mag;
     let depth = feature.geometry.coordinates[2];
-    let circleSize = Math.sqrt(mag)*5;
-    
     return L.circleMarker(coords, {
-        radius: circleSize,
-        fillColor: depth,
+        radius: mag * 4,
+        fillColor: colorScale(depth),
         color: "white",
         weight: 1,
-        oacity: 1,
+        opacity: 1,
         fillOpacity: 0.5
-    })}
+    });
+    //circleMarker.bindPopup(`Magnitude: ${mag} <br> Depth: ${depth}`);
+    //circleMarker.on('click', function(event){
+       // circleMarker.openPopup();})
+}}).addTo(myMap);
   }
-  ).addTo(myMap)
+function colorScale(depth){
+if(depth > 90) { return "#FF3300"  }
+else if(depth > 70) { return "#FF9966" }
+else if(depth > 50) {return "#FFCC66"}
+else if(depth > 30) {return "#CCFF66"}
+else if(depth > 10) {return "#66FF33"}
+else  {return "#006400"}
 }
+
+})
+  //layer.on({Click: function(event){
+   // layer=event.target,
+   // layer.bindPopup(`Magnitude: ${mag} <br> Depth: ${depth}`)
+ // }}).addTo(myMap)
